9a59af9d93f471abba5a92ca84cb999b745b492c
Adam Bernacki <a.bernacki99@gmail.com>
2024-09-16 23:38:52 +0200

    Added pagination to employee page

--- src/main/java/com/bernacki/hrapp/controller/EmployeeController.java
+++ src/main/java/com/bernacki/hrapp/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.bernacki.hrapp.model.ProjectAssignment;
 import com.bernacki.hrapp.service.EmployeeService;
 import com.bernacki.hrapp.service.ProjectAssignmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/employee")
@@ -26,10 +28,26 @@ public class EmployeeController {
         this.projectAssignmentService = projectAssignmentService;
     }
 
+//    @GetMapping("/list")
+//    public String getEmployeeList(Model model){
+//        model.addAttribute( "employees",employeeService.findAll());
+//        return "employee-list";
+//    }
+
     @GetMapping("/list")
-    public String getEmployeeList(Model model){
-        model.addAttribute( "employees",employeeService.findAll());
-        return "employee-list";
+    public String getEmployeeList(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            Model model){
+        Page<Employee> employeePage = employeeService.findAllPaginated(page, size);
+
+        List<Integer> pageNumbers = IntStream.rangeClosed(1, employeePage.getTotalPages())
+                        .boxed().toList();
+        model.addAttribute("employeePage", employeePage);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", employeePage.getTotalPages());
+        model.addAttribute("pageNumbers", pageNumbers);
+        return "employee/employee-list";
     }
 
     @GetMapping("/info")
@@ -38,6 +56,6 @@ public class EmployeeController {
         List<ProjectAssignment> assignments = projectAssignmentService.findProjectsAssignedToEmployeeWithRolesById(id);
         model.addAttribute("assignments", assignments);
         model.addAttribute("employee", employee);
-        return "employee-info";
+        return "employee/employee-info";
     }
 }
